61c5faede52b30e2b08bd423081fe382887fb1aa
Bryan Guillaume <bryan.guillaume@doct.ulg.ac.be>
2015-05-12 12:50:07 +0100

    Add code to handle new tests
    Careful!
    - Work still in progress for F-tests of rank >1
    - Need to test if it is encoded properly

--- swe_contrasts.m
+++ swe_contrasts.m
@@ -42,8 +42,11 @@ end
 Vbeta = SwE.Vbeta;
 Vcov_beta = SwE.Vcov_beta;
 dof_type = SwE.dof.dof_type;
-if dof_type
+if dof_type == 1
     Vcov_beta_g = SwE.Vcov_beta_g;
+end
+if dof_type>1
+    Vcov_vis = SwE.Vcov_vis;
 end
 
 %-Compute & store contrast parameters, contrast/ESS images, & SwE images
@@ -135,7 +138,7 @@ for i = 1:length(Ic)
         it = 0;
         it2 = 0;
         cCovBc = zeros(size(Co,2)*(size(Co,2)+1)/2,S);
-        if dof_type
+        if dof_type == 1
             cCovBc_g = zeros(size(Co,2)*(size(Co,2)+1)/2,S,SwE.Gr.nGr);
         else
             xCon(ic).edf = sum(SwE.dof.nSubj_dof(unique(SwE.dof.iBeta_dof(ind))) - ...
@@ -144,7 +147,7 @@ for i = 1:length(Ic)
         for j = 1:size(Co,1)
             for jj = j:size(Co,1)
                 it = it + 1;
-                if any(j == ind) & any(jj == ind)
+                if any(j == ind) && any(jj == ind)
                     it2 = it2+1;
                     weight = Co(j,:)'*Co(jj,:);
                     if (j~=jj) %was wrong (BG - 13/09/13) 
@@ -152,7 +155,7 @@ for i = 1:length(Ic)
                     end
                     weight = weight(tril(ones(size(Co,2)))==1);
                     cCovBc = cCovBc + weight * spm_get_data(Vcov_beta(it),XYZ);
-                    if dof_type
+                    if dof_type == 1
                         for g = 1:SwE.Gr.nGr                            
                             cCovBc_g(:,:,g) = cCovBc_g(:,:,g) + weight *...
                                 spm_get_data(Vcov_beta_g((g-1)*nCov_beta+it),XYZ);
@@ -174,27 +177,60 @@ for i = 1:length(Ic)
                 eSTAT = 'Z';
                 Z = cB ./ sqrt(cCovBc);
                 spm_progress_bar('Set',100*(0.1));
-                if dof_type                   
-                    tmp = 0;
-                    for g = 1:SwE.Gr.nGr
-                        tmp = tmp + cCovBc_g(:,:,g).^2/SwE.dof.edof_Gr(g);
-                        spm_progress_bar('Set',100*(g/SwE.Gr.nGr/10+0.1));
-                    end
-                    clear cCovBc_g
-                    edf = cCovBc.^2 ./ tmp;
-                    spm_progress_bar('Set',100*(0.2));
-                    % transform into Z-scores image
-                    Z2(Z>0) = -norminv(tcdf(-Z(Z>0),edf(Z>0))); 
-                    Z2(Z<0) = norminv(tcdf(Z(Z<0),edf(Z<0))); 
-                    %Z = -log10(1-spm_Tcdf(Z,edf)); %transfo into -log10(p)
-                    spm_progress_bar('Set',100);
-                else
-                    % transform into Z-scores image
-                    Z2(Z>0) = -norminv(tcdf(-Z(Z>0),xCon(ic).edf)); 
-                    Z2(Z<0) = norminv(tcdf(Z(Z<0),xCon(ic).edf));
-                    % transform into -log10(p-values) image
-                    %Z = -log10(1-spm_Tcdf(Z,xCon(ic).edf));
-                    spm_progress_bar('Set',100);
+                switch dof_type 
+                    case 1
+                        tmp = 0;
+                        for g = 1:SwE.Gr.nGr
+                            tmp = tmp + cCovBc_g(:,:,g).^2/SwE.dof.edof_Gr(g);
+                            spm_progress_bar('Set',100*(g/SwE.Gr.nGr/10+0.1));
+                        end
+                        clear cCovBc_g
+                        edf = cCovBc.^2 ./ tmp;
+                        spm_progress_bar('Set',100*(0.2));
+                        % transform into Z-scores image
+                        Z2(Z>0) = -norminv(tcdf(-Z(Z>0),edf(Z>0))); 
+                        Z2(Z<0) = norminv(tcdf(Z(Z<0),edf(Z<0))); 
+                        %Z = -log10(1-spm_Tcdf(Z,edf)); %transfo into -log10(p)
+                        spm_progress_bar('Set',100);
+                    case 0
+                        % transform into Z-scores image
+                        Z2(Z>0) = -norminv(tcdf(-Z(Z>0),xCon(ic).edf)); 
+                        Z2(Z<0) = norminv(tcdf(Z(Z<0),xCon(ic).edf));
+                        % transform into -log10(p-values) image
+                        %Z = -log10(1-spm_Tcdf(Z,xCon(ic).edf));
+                        spm_progress_bar('Set',100);
+                    case 2
+                        CovcCovBc = 0;
+                        for g = 1:SwE.Gr.nGr
+                            Wg = kron(Co,Co)' * swe_duplication_matrix(size(Co,1)) * SwE.Vis.weight(:,SwE.Vis.iGr_Cov_vis_g==g);
+                            Wg = kron(Wg,Wg) * swe_duplication_matrix(SwE.Vis.nCov_vis_g(g));
+                            CovcCovBc = CovcCovBc + Wg * swe_vechCovVechV(spm_get_data(Vcov_vis(SwE.Vis.iGr_Cov_vis_g==g),XYZ),SwE.dof.dofMat{g},1);
+                            spm_progress_bar('Set',100*(0.1) + g*80/SwE.Gr.nGr);
+                        end
+                        clear Wg
+                        edf = 2 * cCovBc.^2 ./ CovcCovBc - 2; 
+                        clear CovcCovBc
+                        % transform into Z-scores image
+                        Z2(Z>0) = -norminv(tcdf(-Z(Z>0),edf(Z>0))); 
+                        Z2(Z<0) = norminv(tcdf(Z(Z<0),edf(Z<0))); 
+                        %Z = -log10(1-spm_Tcdf(Z,edf)); %transfo into -log10(p)
+                        spm_progress_bar('Set',100);
+                    case 3
+                        CovcCovBc = 0;
+                        for g = 1:SwE.Gr.nGr
+                            Wg = kron(Co,Co)' * swe_duplication_matrix(size(Co,1)) * SwE.Vis.weight(:,SwE.Vis.iGr_Cov_vis_g==g);
+                            Wg = kron(Wg,Wg) * swe_duplication_matrix(SwE.Vis.nCov_vis_g(g));
+                            CovcCovBc = CovcCovBc + Wg * swe_vechCovVechV(spm_get_data(Vcov_vis(SwE.Vis.iGr_Cov_vis_g==g),XYZ),SwE.dof.dofMat{g},2);
+                            spm_progress_bar('Set',100*(0.1) + g*80/SwE.Gr.nGr);                            
+                        end  
+                        clear Wg
+                        edf = 2 * cCovBc.^2 ./ CovcCovBc;
+                        clear CovcCovBc
+                        % transform into Z-scores image
+                        Z2(Z>0) = -norminv(tcdf(-Z(Z>0),edf(Z>0))); 
+                        Z2(Z<0) = norminv(tcdf(Z(Z<0),edf(Z<0))); 
+                        %Z = -log10(1-spm_Tcdf(Z,edf)); %transfo into -log10(p)
+                        spm_progress_bar('Set',100);
                 end               
                 
             case 'F'                                 %-Compute spm{F} image
@@ -203,52 +239,106 @@ for i = 1:length(Ic)
                 if size(Co,2)==1
                     Z = abs(cB ./ sqrt(cCovBc));
                     spm_progress_bar('Set',100*(0.1));
-                    if dof_type
-                        tmp = 0;
-                        for g = 1:SwE.Gr.nGr
-                            tmp = tmp + cCovBc_g(:,:,g).^2/SwE.dof.edof_Gr(g);
-                            spm_progress_bar('Set',100*(g/SwE.Gr.nGr/10+0.1));
-                        end
-                        clear cCovBc_g
-                        edf = cCovBc.^2 ./ tmp;
-                        spm_progress_bar('Set',100*(3/4));
-                        % transform into X-scores image
-                        Z2 = (norminv(spm_Tcdf(-abs(Z),edf))).^2;
-                        % transform into -log10(p-values) image
-                        %Z = -log10(1-spm_Fcdf(Z,1,edf));
-                        spm_progress_bar('Set',100);
-                    else
-                        % transform into X-scores image
-                        Z2 = (norminv(spm_Tcdf(-abs(Z),xCon(ic).edf))).^2;
-                        % transform into -log10(p-values) image
-                        %Z = -log10(1-spm_Fcdf(Z,1, xCon(ic).edf));
-                        spm_progress_bar('Set',100);
+                    switch dof_type
+                        case 1
+                            tmp = 0;
+                            for g = 1:SwE.Gr.nGr
+                                tmp = tmp + cCovBc_g(:,:,g).^2/SwE.dof.edof_Gr(g);
+                                spm_progress_bar('Set',100*(g/SwE.Gr.nGr/10+0.1));
+                            end
+                            clear cCovBc_g
+                            edf = cCovBc.^2 ./ tmp;
+                            spm_progress_bar('Set',100*(3/4));
+                            % transform into X-scores image
+                            Z2 = (norminv(spm_Tcdf(-abs(Z),edf))).^2;
+                            % transform into -log10(p-values) image
+                            %Z = -log10(1-spm_Fcdf(Z,1,edf));
+                            spm_progress_bar('Set',100);
+                        case 0
+                            % transform into X-scores image
+                            Z2 = (norminv(spm_Tcdf(-abs(Z),xCon(ic).edf))).^2;
+                            % transform into -log10(p-values) image
+                            %Z = -log10(1-spm_Fcdf(Z,1, xCon(ic).edf));
+                            spm_progress_bar('Set',100);
+                        case 2
+                            CovcCovBc = 0;
+                            for g = 1:SwE.Gr.nGr
+                                Wg = kron(Co,Co)' * swe_duplication_matrix(size(Co,1)) * SwE.Vis.weight(:,SwE.Vis.iGr_Cov_vis_g==g);
+                                Wg = kron(Wg,Wg) * swe_duplication_matrix(SwE.Vis.nCov_vis_g(g));
+                                CovcCovBc = CovcCovBc + Wg * swe_vechCovVechV(spm_get_data(Vcov_vis(SwE.Vis.iGr_Cov_vis_g==g),XYZ),SwE.dof.dofMat{g},1);
+                                spm_progress_bar('Set',100*(g/SwE.Gr.nGr/10+0.1));
+                            end
+                            clear Wg
+                            edf = 2 * cCovBc.^2 ./ CovcCovBc - 2; 
+                            clear CovcCovBc
+                            spm_progress_bar('Set',100*(3/4));
+                            % transform into X-scores image
+                            Z2 = (norminv(spm_Tcdf(-abs(Z),edf))).^2;
+                            % transform into -log10(p-values) image
+                            %Z = -log10(1-spm_Fcdf(Z,1,edf));
+                            spm_progress_bar('Set',100);
+                        case 3
+                            CovcCovBc = 0;
+                            for g = 1:SwE.Gr.nGr
+                                Wg = kron(Co,Co)' * swe_duplication_matrix(size(Co,1)) * SwE.Vis.weight(:,SwE.Vis.iGr_Cov_vis_g==g);
+                                Wg = kron(Wg,Wg) * swe_duplication_matrix(SwE.Vis.nCov_vis_g(g));
+                                CovcCovBc = CovcCovBc + Wg * swe_vechCovVechV(spm_get_data(Vcov_vis(SwE.Vis.iGr_Cov_vis_g==g),XYZ),SwE.dof.dofMat{g},2);
+                                spm_progress_bar('Set',100*(g/SwE.Gr.nGr/10+0.1));                           
+                            end  
+                            clear Wg
+                            edf = 2 * cCovBc.^2 ./ CovcCovBc;
+                            % transform into X-scores image
+                            Z2 = (norminv(spm_Tcdf(-abs(Z),edf))).^2;
+                            % transform into -log10(p-values) image
+                            %Z = -log10(1-spm_Fcdf(Z,1,edf));
+                            spm_progress_bar('Set',100);
+                            clear CovcCovBc
                     end
+                    Z = Z.^2;
                 else
                     Z   = zeros(1,S);
-                    if dof_type
+                    if dof_type ~= 0
                         edf = zeros(1,S);
+                    end
+                    if dof_type == 2
+                        CovcCovBc = 0;
+                        for g = 1:SwE.Gr.nGr
+                             Wg = kron(Co,Co)' * swe_duplication_matrix(size(Co,1)) * SwE.Vis.weight(:,SwE.Vis.iGr_Cov_vis_g==g);
+                             Wg = sum(kron(Wg,Wg)) * swe_duplication_matrix(SwE.Vis.nCov_vis_g(g));
+                             CovcCovBc = CovcCovBc + Wg * swe_vechCovVechV(spm_get_data(Vcov_vis(SwE.Vis.iGr_Cov_vis_g==g),XYZ),SwE.dof.dofMat{g},1);
+                        end
+                        edf = 2 * (sum(swe_duplication_matrix(size(Co,2))) * cCovBc).^2 ./ CovcCovBc - 2;
                     end
                     for iVox=1:S
                         cCovBc_vox = zeros(size(Co,2));
                         cCovBc_vox(tril(ones(size(Co,2)))==1) = cCovBc(:,iVox);
                         cCovBc_vox = cCovBc_vox + cCovBc_vox' - diag(diag(cCovBc_vox));
                         Z(iVox) = cB(:,iVox)' / cCovBc_vox * cB(:,iVox);                     
-                        if dof_type
-                            tmp = 0;
-                            for g = 1:SwE.Gr.nGr
-                                cCovBc_g_vox = zeros(size(Co,2));
-                                cCovBc_g_vox(tril(ones(size(Co,2)))==1) = cCovBc_g(:,iVox,g);
-                                cCovBc_g_vox = cCovBc_g_vox + cCovBc_g_vox' - diag(diag(cCovBc_g_vox));
-                                tmp = tmp + (trace(cCovBc_g_vox^2) + (trace(cCovBc_g_vox))^2)/...
-                                    SwE.dof.edof_Gr(g);                              
-                            end
-                            edf(iVox)=(trace(cCovBc_vox^2) + (trace(cCovBc_vox))^2) / tmp;
+                        switch dof_type
+                            case 1
+                                tmp = 0;
+                                for g = 1:SwE.Gr.nGr
+                                    cCovBc_g_vox = zeros(size(Co,2));
+                                    cCovBc_g_vox(tril(ones(size(Co,2)))==1) = cCovBc_g(:,iVox,g);
+                                    cCovBc_g_vox = cCovBc_g_vox + cCovBc_g_vox' - diag(diag(cCovBc_g_vox));
+                                    tmp = tmp + (trace(cCovBc_g_vox^2) + (trace(cCovBc_g_vox))^2)/...
+                                        SwE.dof.edof_Gr(g);                              
+                                end
+                                edf(iVox)=(trace(cCovBc_vox^2) + (trace(cCovBc_vox))^2) / tmp;
+                            case 2
+                                CovcCovBc = 0;
+                                for g = 1:SwE.Gr.nGr
+                                    Wg = kron(Co,Co)' * swe_duplication_matrix(size(Co,1)) * SwE.Vis.weight(:,SwE.Vis.iGr_Cov_vis_g==g);
+                                    Wg = sum(kron(Wg,Wg)) * swe_duplication_matrix(SwE.Vis.nCov_vis_g(g));
+                                    CovcCovBc = CovcCovBc + Wg * swe_vechCovVechV(spm_get_data(Vcov_vis(SwE.Vis.iGr_Cov_vis_g==g),XYZ),SwE.dof.dofMat{g},1);
+                                end
+                            edf = 2 * cCovBc.^2 ./ CovcCovBc - 2;
+                            case 3
                             
                         end
                         spm_progress_bar('Set',100*(iVox/S/2));
                     end
-                    if dof_type
+                    if dof_type ~= 0
                         clear cCovBc_g
                         Z = Z .*(edf-xCon(ic).eidf+1)./edf/xCon(ic).eidf;
                         % transform into X-scores image
@@ -261,8 +351,8 @@ for i = 1:length(Ic)
                         Z2 = chi2inv(spm_Fcdf(Z,xCon(ic).eidf,xCon(ic).edf),1);
                         % transform into -log10(p-values) image
                         %Z = -log10(1-spm_Fcdf(Z,xCon(ic).eidf,xCon(ic).edf));
-                        spm_progress_bar('Set',100);
                     end
+                    spm_progress_bar('Set',100);
                 end
         end
         spm_progress_bar('Clear')
