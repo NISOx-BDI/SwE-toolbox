e33703eb363e9a574b0e05bdc6cb37c82b0915a9
Bryan Guillaume <bryan.guillaume@doct.ulg.ac.be>
2016-12-02 15:12:12 +0800

    add a new function that thresholds images
    The function keeps only voxels with values >= threshold (so inclusive
    threshold) and in a cluster of size >= minimumClusterSize

---
+++ swe_tresholdImage.m
@@ -0,0 +1,59 @@
+function swe_tresholdImage(threshold, minimumClusterSize)
+% Threshold an image that need to be selected by the user from a selection
+% window
+% FORMAT spm_read_vols(threshold, minimumClusterSize)
+% threshold - minimum value (inclusive) of the surviving voxels
+% minimumClusterSize - the minimum size ofthe surviving clusters
+%
+  inputImageName = spm_select(1, 'image');
+  [pth, bnm, ext] = spm_fileparts(inputImageName);
+  VI = spm_vol(inputImageName);
+  [Z, XYZ] = spm_read_vols(VI);
+  XYZ = inv(VI.mat) * [XYZ; ones(1,VI.dim(1)*VI.dim(2)*VI.dim(3))];
+  XYZ = XYZ(1:3,:);
+
+  Z = spm_get_data(inputImageName, XYZ);
+  VI.fname = fullfile(pth, [bnm '_thresholded' ext]);
+  VI.descrip = [VI.descrip sprintf(' thresholdValue: %fminimumClusterSize: %i', threshold, minimumClusterSize)];
+  VI = spm_create_vol(VI);
+
+  %-Calculate height threshold filtering
+  %--------------------------------------------------------------------------
+  Q      = find(Z >= threshold);
+
+  %-Apply height threshold
+  %--------------------------------------------------------------------------
+  Z      = Z(:,Q);
+  XYZ    = XYZ(:,Q);
+  if isempty(Q)
+    fprintf('\n');                                                      %-#
+    warning('SwE:NoVoxels','No voxels survive the thresholding');
+  else
+
+    %-Calculate extent threshold filtering
+    %----------------------------------------------------------------------
+    A     = spm_clusters(XYZ);
+    Q     = [];
+    for i = 1:max(A)
+      j = find(A == i);
+      if length(j) >= minimumClusterSize; Q = [Q j]; end
+    end
+
+    % ...eliminate voxels
+    %----------------------------------------------------------------------
+    Z     = Z(:,Q);
+    XYZ   = XYZ(:,Q);
+    if isempty(Q)
+      fprintf('\n');                                                  %-#
+      warning('SwE:NoVoxels','No voxels survive the thresholding');
+    end
+
+  end
+
+  tmp= nan(VI.dim);
+  Q = cumprod([1,VI.dim(1:2)])*XYZ - ...
+    sum(cumprod(VI.dim(1:2)));
+  tmp(Q) = Z;
+  spm_write_vol(VI, tmp);
+
+
