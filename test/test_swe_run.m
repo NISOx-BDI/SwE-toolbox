fd891381573b6bbbbe12db0603cd287f9d9d8364
Thomas Maullin-Sapey <tommaullin@gmail.com>
2018-04-30 09:19:25 +0100

    Testing branch started

---
+++ test/test_swe_run.m
@@ -0,0 +1,24 @@
+%==========================================================================
+%Unit tests for testing whether all use cases run in the SwE toolbox. To 
+%run the below run the runTest function. 
+%
+%Author: Thomas Maullin.
+%==========================================================================
+classdef test_swe_run < matlab.unittest.TestCase
+    
+    methods
+        
+        function delete_prev_run(testCase, data_path)
+            
+        end
+        
+    end
+    
+    methods(Test)
+       
+        function test_blah_runs(testCase)
+            
+        end
+        
+    end
+end
