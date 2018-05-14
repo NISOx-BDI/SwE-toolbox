015b60c463b29ac298c50138871450b91de9b575
Thomas Maullin-Sapey <tommaullin@gmail.com>
2018-05-14 14:37:00 +0100

    swe_cp updated to use swe_create_vol

--- swe_create_vol.m
+++ swe_create_vol.m
@@ -2,19 +2,25 @@ function vol=swe_create_vol(fname, dim, m, varargin)
 % FORMAT vol = swe_create_vol(fname, dim, m [, desc])
 % Initialise a new volume for writing
 % 
-% fname  - Filename of new image
-% dim    - Row vector giving image dimensions
-% m      - 4x4 homogeneous transformation, from V.mat
-% desc   - Description to enter into image header
+% fname    - Filename of new image
+% dim      - Row vector giving image dimensions
+% m        - 4x4 homogeneous transformation, from V.mat
+% desc     - Description to enter into image header
+% meshData - Boolean stating whether we output gifti or not.
 %
 %_______________________________________________________________________
 % SwE-toolbox
-
     
 if nargin > 3
     descrip = varargin{1};
 else
     descrip = '';
+end
+
+if nargin > 4
+    meshData = varargin{2};
+else
+    meshData = false;
 end
 
 vol = deal(struct(...
@@ -24,7 +30,12 @@ vol = deal(struct(...
   'mat',      m,...
   'pinfo',    [1 0 0]',...
   'descrip',  descrip));
-vol = spm_create_vol(vol);
+
+if meshData
+    vol = spm_data_hdr_write(vol);
+else
+    vol = spm_create_vol(vol);
+end
 
 
 
