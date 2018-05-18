d576e3d92f6289c0445d62d4279ccef24be476fe
tommaullin <tommaullin@gmail.com>
2018-05-18 13:44:55 +0100

    wip maybe first travis test

---
+++ runWB.sh
@@ -0,0 +1,5 @@
+mv code/spm12/\@file_array/private/mat2file.mex .
+rm code/spm12/\@file_array/private/mat2file.* 
+mv ./mat2file.mex code/spm12/\@file_array/private/
+cd swe
+octave --no-window-system --eval "addpath(genpath(pwd)); load('SwE.mat'); swe_cp_WB(SwE);"
