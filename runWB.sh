030e7c6892e268f1e00ad79cad347fa72fd7fddb
TomMaullin <tommaullin@gmail.com>
2018-05-21 13:26:19 +0100

    starting to get moxunit up and running

--- runWB.sh
+++ runWB.sh
@@ -1,7 +1,15 @@
+# Octave-SPM bug fix
 mv code/spm12/\@file_array/private/mat2file.mex .
 rm code/spm12/\@file_array/private/mat2file.* 
 mv ./mat2file.mex code/spm12/\@file_array/private/
 cd swe
+
+# Testing install
+cd MOxUnit
+make install
+cd ..
+
+# Octave commands
 octave --no-window-system --eval \
 "addpath(genpath(pwd));"\
 "load('test/data/seed.mat');"\
