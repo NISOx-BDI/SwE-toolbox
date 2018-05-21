146c165ea43ec3d028fec52037bf575807a977cd
TomMaullin <tommaullin@gmail.com>
2018-05-21 13:09:29 +0100

    updated to reset seed

--- runWB.sh
+++ runWB.sh
@@ -2,5 +2,11 @@ mv code/spm12/\@file_array/private/mat2file.mex .
 rm code/spm12/\@file_array/private/mat2file.* 
 mv ./mat2file.mex code/spm12/\@file_array/private/
 cd swe
-octave --no-window-system --eval "addpath(genpath(pwd));load('test/data/design.mat');swe_run_design(design);load('SwE.mat');swe_cp_WB(SwE);"
-
+octave --no-window-system --eval \
+"addpath(genpath(pwd));"\
+"load('test/data/seed.mat')"\
+"rand('state','seed')"\
+"load('test/data/design.mat');"\
+"swe_run_design(design);"\
+"load('SwE.mat');"\
+"swe_cp_WB(SwE);"
