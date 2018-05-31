a8838fd98b79421983ced8be2e7fc0cb7559cf8c
TomMaullin <tommaullin@gmail.com>
2018-05-31 15:21:26 +0100

    first test should now hopefully work

--- test/test_swe_run2.m
+++ test/test_swe_run2.m
@@ -1,4 +1,4 @@
-function test_suite=my_test_of_abs
+function test_suite=test_swe_run2
     try % assignment of 'localfunctions' is necessary in Matlab >= 2016
         test_functions=localfunctions();
     catch % no problem; early Matlab versions can use initTestSuite fine
@@ -6,14 +6,56 @@ function test_suite=my_test_of_abs
     initTestSuite;
 end
 
-function verifyMapsUnchanged(testLocation)
-
+function mapsEqual = verifyMapsUnchanged()
 	
+	% List all niftis
+	files = ls("*.nii");
+
+	% store whether maps are equal.
+	equalMaps = [];
+
+	% Compare each file to ground truth
+	for i = 1:size(files, 1)
+
+		% Get the filenames
+		file = files(1, :);
+		gt_file = ['ground_truth' filesep file];
+		disp(['Testing file: ' file])
+
+		% Read in the volumes
+		file = spm_vol(file);
+		file = spm_read_vols(file);
+		gt_file = spm_vol(gt_file);
+		gt_file = spm_read_vols(gt_file);
+
+		% Remove NaN values
+		file = file(~isnan(file));
+		gt_file = gt_file(~isnan(gt_file));
+
+		% Check whether the remaining values are equal.
+		result = ~any(file~=gt_file);
+
+		if result
+			disp('PASSED');
+		else
+			disp('FAILED');
+		end
+
+		% Record result
+		equalMaps = [equalMaps result];
+
+    end
+
+    % return true if all maps were equal false otherwise.
+
+    mapsEqual = ~any(~equalMaps);
 
 end
 
-function test_example
+function test_wb_t_img()
 	
+	disp('Test case running: wb_t_img')
+
 	% Move into the test folder and add the path to it.
 	cd('/swe/test/data/test_wb_t_img');
 	addpath(genpath('/swe/'));
@@ -30,5 +72,8 @@ function test_example
 	load('SwE.mat');
 	swe_cp_WB(SwE);
 
+	% Check against ground truth.
+	mapsUnchanged = verifyMapsUnchanged('/swe/test/data/test_wb_t_img');
+	assertEqual(mapsUnchanged, true);
 	
 end
