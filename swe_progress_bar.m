62585c3ac74a61f6909a5e831bd016f914620368
Thomas Maullin-Sapey <tommaullin@gmail.com>
2018-08-28 13:21:17 +0100

    reviewed swe_progress_bar

--- swe_progress_bar.m
+++ swe_progress_bar.m
@@ -1,13 +1,23 @@
-% ===============================================================
-%
+% =========================================================================
 % This function was added for testing purposes and prevents the 
-% spm_progress_bar displaying in octave. The progress bar can be 
-% used in octave but it creates ascii art which spams the test
-% logs. 
+% spm_progress_bar displaying in octave. The progress bar can be used in 
+% octave but it creates ascii art which spams the test logs. 
+% =========================================================================
+% FORMAT spm_progress_bar('Set',value)
+% Set the height of the bar itself.
 %
+% FORMAT spm_progress_bar('Set','xlabel',xlabel)
+% FORMAT spm_progress_bar('Set','ylabel',ylabel)
+% Set the progress bar labels.
+%
+% FORMAT spm_progress_bar('Set','height',height)
+% Set the height of the progress bar.
+%
+% FORMAT spm_progress_bar('Clear')
+% Clear the 'Interactive' window.
+% =========================================================================
 % Author: Tom Maullin (08/06/2018)
-%
-% ===============================================================
+% =========================================================================
 function swe_progress_bar(varargin)
 
     if ~exist('OCTAVE_VERSION','builtin')
