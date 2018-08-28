0ef44d76ae29822218d761db24668706b90eda37
Thomas Maullin-Sapey <tommaullin@gmail.com>
2018-08-28 13:46:53 +0100

    reviewed swe_design

--- swe_design.m
+++ swe_design.m
@@ -1,16 +1,20 @@
-function swe_design
+% =========================================================================
 % Sandwich Estimator for Neuroimaging Longitudinal Data Analysis, SwE.
 %
-% This function prepares (if needed) and launches the batch system with a job containing the batch module for the specification of
+% This function prepares (if needed) and launches the batch system with a 
+% job containing the batch module for the specification of
 % data and design.
-
+% =========================================================================
 % Written by Bryan Guillaume
+function swe_design
 
-% Initiate a job 
-spm_jobman('initcfg')
-% Launch the batch system with the SwE tab
-swe_batch
-% Add the specification module to it
-spm_jobman('interactive','','swe.design');
+    % Initiate a job 
+    spm_jobman('initcfg')
+    % Launch the batch system with the SwE tab
+    swe_batch
+    % Add the specification module to it
+    spm_jobman('interactive','','swe.design');
 
-return
+    return
+    
+end
