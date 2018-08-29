776580e7ba56c8400d9b37b6c5620bc4c77cf90d
TomMaullin <tommaullin@gmail.com>
2018-08-29 14:34:45 +0100

    swe_cfg_batch reviewed

--- swe_cfg_batch.m
+++ swe_cfg_batch.m
@@ -1,19 +1,24 @@
-function swe = swe_cfg_batch
-% Sandwich Estimator Method for Neuroimaging Longitudinal Data Analysis, SwE.
-% SwE configuration file
+% ====================================================================
+% Sandwich Estimator Method for Neuroimaging Longitudinal Data 
+% Analysis, SwE. SwE configuration file.
+%
 % This builds the whole tree for the various tools and their GUI.
-
+% ====================================================================
+% FORMAT swe = swe_cfg_batch
+% ====================================================================
 % Written by Bryan Guillaume
 
-swe         = cfg_choice;
-swe.tag     = 'swe';
-swe.name    = 'SwE';
-swe.help    = {[...
-    'This is the batch interface for SwE, i.e. Sandwich Estimator '...
-    'Method for Neuroimaging Longitudinal Data Analysis.']
-                  }';
-swe.values  = {swe_cfg_design};
-              
-%------------------------------------------------------------------------
+function swe = swe_cfg_batch
 
-return
+	swe         = cfg_choice;
+	swe.tag     = 'swe';
+	swe.name    = 'SwE';
+	swe.help    = {[...
+	    'This is the batch interface for SwE, i.e. Sandwich Estimator '...
+	    'Method for Neuroimaging Longitudinal Data Analysis.']
+	                  }';
+	swe.values  = {swe_cfg_design};
+	              
+	%------------------------------------------------------------------------
+
+end
