3f3663da9f1609eeea16b787baf871e3319edceb
Thomas Maullin-Sapey <tommaullin@gmail.com>
2018-09-04 13:29:24 +0100

    comment in batch updated

--- swe_cfg_design.m
+++ swe_cfg_design.m
@@ -131,7 +131,7 @@ cov.val     = {{''}};
 cov.help    = {
                'Select the *.mat/*.txt file(s) containing details of your multiple covariates. '
                ''
-               'You will first need to create a *.mat file containing a matrix R or a *.txt file containing the covariates. Each column of R will contain a different covariate. Unless the covariates names are given in a cell array called ''names'' in the MAT-file containing variable R, the covariates will be named R1, R2, R3, ..etc.'
+               'A *.mat file must contain a matrix R, and may optionally contain a cell array called ''names'' with covariate names. A *.txt file can be any file that can be read by Matlab''s IMPORTDATA function, which includes a data-only text file, with spaces, tabs or comma separators. A header row is allowed, and if present will be used to obtain the covariate names. If names for the covariates are not give they will be named R1, R2, R3, ..etc.'
               }';
 cov.filter  = 'mat';
 cov.ufilter = '.*';
