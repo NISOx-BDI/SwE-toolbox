258f5314286beb0df84d77fa71b1cdac60c84ace
nicholst <t.e.nichols@warwick.ac.uk>
2018-10-02 09:31:18 +0100

    Another missing FORMAT output

--- swe_vechCovVechV.m
+++ swe_vechCovVechV.m
@@ -1,7 +1,7 @@
 function vechCovVechV = swe_vechCovVechV(covVis,dofMat,type)
 % Compute vech(Cov(vech(\hat V))) following one of 3 input types.
 % =========================================================================
-% FORMAT: swe_vechCovVechV(covVis,dofMat,type)
+% FORMAT: vechCovVechV = swe_vechCovVechV(covVis,dofMat,type)
 % -------------------------------------------------------------------------
 % Inputs:
 %
