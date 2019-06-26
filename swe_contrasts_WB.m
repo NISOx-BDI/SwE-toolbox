273061ace4b0bdd80d7fe5066bad6087e7e8d609
Bryan Guillaume <bryan.guillaume@hotmail.com>
2019-06-26 11:41:37 +0200

    add a space for consistency

--- swe_contrasts_WB.m
+++ swe_contrasts_WB.m
@@ -65,7 +65,7 @@ function [SwE] = swe_contrasts_WB(SwE)
     % Add the SwE volumes.
     %----------------------------------------------------------------------
     DxCon.Vspm = spm_vol(sprintf('swe_vox_%c%cstat_c%.2d%s', eSTAT, STAT, 1, file_ext));
-    DxCon.VspmUncP = spm_vol(sprintf('swe_vox_%cstat_lp%s_c%.2d%s', STAT, wbstring,1, file_ext));
+    DxCon.VspmUncP = spm_vol(sprintf('swe_vox_%cstat_lp%s_c%.2d%s', STAT, wbstring, 1, file_ext));
     DxCon.VspmFDRP = spm_vol(sprintf('swe_vox_%cstat_lpFDR%s_c%.2d%s', STAT, wbstring, 1, file_ext));
     DxCon.VspmFWEP = spm_vol(sprintf('swe_vox_%cstat_lpFWE%s_c%.2d%s', STAT, wbstring, 1, file_ext));
     if isfield(SwE,'WB')
