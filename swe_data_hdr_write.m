d6ed157a9871b43c9eafbc782f433e6f3c76db35
Bryan Guillaume <bryan.guillaume@hotmail.com>
2019-10-01 23:12:51 +0200

    Modify the way CIfTI files are created

--- swe_data_hdr_write.m
+++ swe_data_hdr_write.m
@@ -28,10 +28,25 @@ function V = swe_data_hdr_write(fname, DIM, M, descrip, metadata, varargin)
     metadata{:});
   
   if isfield(V, 'ciftiTemplate')
-    copyfile(V.ciftiTemplate, fname);
-    V = swe_data_hdr_read(fname);
+    [~, sliceInd] = swe_get_file_extension(V.ciftiTemplate);
+    if isempty(sliceInd)
+      sourceName = V.ciftiTemplate;
+    else
+      sourceName = V.ciftiTemplate(1:( end - numel(sliceInd) ));
+    end
+    % make sure we select only one slice
+    V = swe_data_hdr_read(sprintf('%s,1',sourceName));
+    V.fname = fname;
+    V.descrip = descrip;
     V.private.dat.fname = fname;
+    V.private.dat = file_array(fname,...
+                                 [1,1,1,1,1,V.dim(1)],...
+                                 V.private.dat.dtype,...
+                                 0,...
+                                 1,...
+                                 0);
     V.private.dat(:) = NaN;
+    create(V.private);
   else
     V = spm_data_hdr_write(V);
   end
