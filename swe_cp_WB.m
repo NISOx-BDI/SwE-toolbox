b0049e62da66c9bc7f9735c05f194a5f3e3f2c8f
Bryan Guillaume <bryan.guillaume@hotmail.com>
2019-10-03 11:29:12 +0200

    Add detection of CIfTI inputs

--- swe_cp_WB.m
+++ swe_cp_WB.m
@@ -86,8 +86,9 @@ end
 
 %-Check if we have data in a.mat format and set some variables accordingly
 %--------------------------------------------------------------------------
-[~,~,file_ext] = fileparts(SwE.xY.P{1});
-isMat          = strcmpi(file_ext,'.mat');
+file_ext = swe_get_file_extension(SwE.xY.P{1});
+isMat    = strcmpi(file_ext,'.mat');
+isCifti  = strcmpi(file_ext,'.dtseries.nii') ||  strcmpi(file_ext,'.dscalar.nii');
 isOctave = exist('OCTAVE_VERSION','builtin');
 
 if ~isMat
