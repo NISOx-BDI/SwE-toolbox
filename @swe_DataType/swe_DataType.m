908a3e8c71af78d07af74856425d1723e8d99841
nicholst <thomas.nichols@bdi.ox.ac.uk>
2020-08-21 13:33:52 +0100

    Replace double quotes, missed from #169

--- @swe_DataType/swe_DataType.m
+++ @swe_DataType/swe_DataType.m
@@ -9,11 +9,11 @@ function obj = swe_DataType(dataType)
   availableDataTypes = {'Nifti', 'Gifti', 'Cifti', 'Mat', 'VolumeMat', 'SurfaceMat'};
 
   if nargin == 0
-    error( "swe_DataType cannot be initialized empty; choose from 'Nifti', 'Gifti', 'Cifti', 'Mat', 'VolumeMat' or 'SurfaceMat'.");
+    error('swe_DataType cannot be initialized empty; choose from "Nifti", "Gifti", "Cifti", "Mat", "VolumeMat" or "SurfaceMat".');
   end
 
   if ~ismember(dataType, availableDataTypes)
-    error("%s is not a valid swe_DataType. Choose from 'Nifti', 'Gifti', 'Cifti', 'Mat', 'VolumeMat' or 'SurfaceMat'.", dataType);
+    error('%s is not a valid swe_DataType. Choose from "Nifti", "Gifti", "Cifti", "Mat", "VolumeMat" or "SurfaceMat".', dataType);
   end
 
   obj.value = dataType;
