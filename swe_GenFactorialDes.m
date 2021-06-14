2280327268461d1efc339ae2e07b6888813ec3a0
nicholst <thomas.nichols@bdi.ox.ac.uk>
2021-06-14 12:10:56 +0100

    Typos

--- swe_GenFactorialDes.m
+++ swe_GenFactorialDes.m
@@ -2,10 +2,10 @@ function [groups,subjects,visits,X] = swe_GenFactorialDes(nVis,nSub)
 % Create key input variables for full factorial design
 % =========================================================================
 % Based on the number of visits and number of subjects per group, this
-% function creates the four essenetial inputs for a SwE analysis:
+% function creates the four essential inputs for a SwE analysis:
 % groups, subjects and visit vectors, and the design matrix X.
 % 
-% Crucially, the function assumes that visit indicies vary fastest, and 
+% Crucially, the function assumes that visit indices vary fastest, and 
 % groups vary slowest (i.e. the first scans are all the visits for first 
 % subject in group 1.)
 %
@@ -18,13 +18,13 @@ function [groups,subjects,visits,X] = swe_GenFactorialDes(nVis,nSub)
 % -------------------------------------------------------------------------
 % Outputs:
 %  - groups: Groups indicator, values ranging from 1 to length(nSub)
-%  - subjects: Subjects indicator, values rangeing from 1 to sum(nSub)
-%  - visits: Visits indicator, values ranging form 1 to nVis
+%  - subjects: Subjects indicator, values ranging from 1 to sum(nSub)
+%  - visits: Visits indicator, values ranging from 1 to nVis
 %  - X: Design matrix, nVis*sum(nSum) rows, nvis*length(nSub) columns
 % =========================================================================
 % 
-% Unbalanced designs can be obtaiend by carefully subsetting the elements
-% of groups, subjects & visits and the corresponding rows of X.
+% Unbalanced designs can be obtained by carefully removing elements of 
+% the groups, subjects & visits vectors and the corresponding rows of X.
 %
 % =========================================================================
 % written by Thomas Nichols
